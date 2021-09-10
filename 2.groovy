dcb1712fd24b702f85005f4a7aa2478c04befc8d
rahymov <y.rahymov@gmail.com>
2021-09-10 00:52:58 +0000

    script added

---
+++ 2.groovy
@@ -0,0 +1,22 @@
+node{
+    stage("Pull"){
+        git 'https://github.com/ikambarov/spring-petclinic.git'
+    }
+    stage("Build"){
+        sh '''
+            echo "Build"
+        '''
+    }
+    stage("Test"){
+        sh '''
+            echo "Test"
+        '''
+        
+        input 'Do you want to continue?'
+    }
+    stage("Deploy"){
+        sh '''
+            echo "Deploy"
+        '''
+    }
+}
