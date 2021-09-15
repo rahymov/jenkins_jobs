710e30a189e1213e65c2e3453a3b24b4fb3fc71f
rahymov <y.rahymov@gmail.com>
2021-09-15 22:36:45 +0000

    add anisble test

---
+++ ansible.groovy
@@ -0,0 +1,17 @@
+properties([
+  parameters([
+    string(description: 'Provide Linux Machine', name: 'IPADDRESS', trim: true)
+  ])
+])
+
+node('worker1'){
+    withCredentials([sshUserPrivateKey(credentialsId: 'masterkey', keyFileVariable: 'SSHKEY', usernameVariable: 'SSHUSERNAME')]) {
+        stage("Test"){
+            sh """
+                export ANSIBLE_HOST_KEY_CHECKING=False
+                ansible --private-key $SSHKEY -i '${params.IPADDRESS},' all -m ping
+            """
+        }
+       
+    } 
+}
