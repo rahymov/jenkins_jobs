c87f678dd883d2586bdbce164955f31b70b279a2
rahymov <y.rahymov@gmail.com>
2021-09-15 22:38:45 +0000

    added melodi app

---
+++ ansible-melodi.groovy
@@ -0,0 +1,22 @@
+properties([
+    parameters([
+        string(description: 'Provide Linux Machine', name: 'IPADDRESS', trim: true)
+        ])
+    ])
+    
+node("worker1"){
+    stage("Pull repo"){
+        git 'https://github.com/ikambarov/ansible-melodi.git'
+
+        sh "ls"
+    }
+
+     withCredentials([sshUserPrivateKey(credentialsId: 'masterkey', keyFileVariable: 'SSHKEY', usernameVariable: 'SSHUSERNAME')]) {
+        stage("Install Melodi"){
+            sh """
+                export ANSIBLE_HOST_KEY_CHECKING=False
+                ansible-playbook --private-key $SSHKEY -i '${params.IPADDRESS},' main.yml
+            """
+        }
+    } 
+}
